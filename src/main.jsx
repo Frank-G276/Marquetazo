6eef6c1b73da4c4e9cc865e2a162d94f1e0e85e5
Frank_G <guilombofrank07@gmail.com>
2025-11-13 17:20:03 -0500

    fix: correct import path for CartContext in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
-import { CartProvider } from './features/shoppingCart/CartContext.jsx'
+import { CartProvider } from './features/ShoppingCart/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
